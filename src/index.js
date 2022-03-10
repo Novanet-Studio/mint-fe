5ded2d218e34aa2a31f80fdf8ee82bf26eca53d7
Freddy Polania <fpolania@gmail.com>
2022-03-10 18:46:30 -0400

    Creación proyecto 1. Creación aplicación react. 2. Creación estructura directorios 3. Instalación scss y edición de hojas de estilo existentes. 4. Organización de vistas, componentes y assets de la aplicación

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import ListedCardPage from "./pages/ListedCardPage";
+import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListedCardPage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("container")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/ListedCardPage.test.js
+++ src/pages/ListedCardPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ListedCardPage from './ListedCardPage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ListedCardPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
